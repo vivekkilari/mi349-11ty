b62edc953a2fd51d795ac35dd58775d52156a506
vivekkilari <vkilariofficial@gmail.com>
2025-12-05 23:06:15 -0500

    More progress

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,24 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+  eleventyConfig.addPassthroughCopy("itemInfo");
 
-  eleventyConfig.addShortcode("item", function(name){
-    return `<div>${name}</div>`;  
+  eleventyConfig.addShortcode("item", function(itemName, imageName, itemInfo){
+      var itemInfoContents = "";
+      fetch(itemInfo)
+        .then((res) => res.text())
+        .then((text) => itemInfoContents = text.toString())
+        .catch((e) => console.log(e));
+
+      return ( `
+          <div class="item">
+          <img src="${imageName}" alt="Image of ${itemName}" class="itemImage">  
+          <div class="itemBlock">
+          <strong class="itemHeader">${itemName}</strong>
+          <p class="itemInfo">${itemInfoContents.toString()}</p>
+          </div>
+          </div>
+          `
+      );
   });
 };
